ece2bb8fc94e1872009c8a233938e017e0ae1ca3
sherif.butt@gmail.com <sherif.butt@gmail.com>
2021-11-23 09:43:12 +0000

    fixed Lottie issue_09

--- components/Header/Header_0.js
+++ components/Header/Header_0.js
@@ -1,4 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
+import Link from "next/link";
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import {
@@ -16,7 +17,7 @@ import {
    XIcon,
 } from "@heroicons/react/outline";
 import { ChevronDownIcon } from "@heroicons/react/solid";
-import Dropdown from '../Dropdown'
+import Dropdown from "../Dropdown";
 
 const solutions = [
    {
@@ -112,15 +113,18 @@ export default function Example() {
       <Popover className="relative bg-white">
          <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-               <div className="flex justify-start lg:w-0 lg:flex-1">
-                  <a href="#">
-                     <span className="sr-only">Workflow</span>
-                     <img
-                        className="h-8 w-auto sm:h-10"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt=""
-                     />
-                  </a>
+               <div className="flex justify-start lg:w-0 lg:flex-1 align-middle">
+                  <Link href="/" passHref>
+                     <a className="inline-flex items-center">
+                        <span className="sr-only">Workflow</span>
+                        <img
+                           className="h-8 w-auto sm:h-10 text-indigo-500"
+                           src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                           alt=""
+                        />
+                        <span className="ml-2 font-size-20  text-2xl text-gray-500 align-middle h-auto inline" >SBOLIO</span>
+                     </a>
+                  </Link>
                </div>
                <div className="-mr-2 -my-2 md:hidden">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -224,11 +228,11 @@ export default function Example() {
                      className="text-base font-medium text-gray-500 hover:text-gray-900">
                      Pricing
                   </a>
-                  <a
-                     href="#"
-                     className="text-base font-medium text-gray-500 hover:text-gray-900">
-                     Docs
-                  </a>
+                  <Link href="/faq" passHref>
+                     <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        Faq
+                     </a>
+                  </Link>
 
                   <Popover className="relative">
                      {({ open }) => (
@@ -343,8 +347,8 @@ export default function Example() {
                      href="#"
                      className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                      Sign up
-             </a>
-             <Dropdown/>
+                  </a>
+                  <Dropdown />
                </div>
             </div>
          </div>
