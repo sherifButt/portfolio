7cbccac84c6bf7162c17c44cd50660f7645c6010
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-01-16 11:25:22 +0000

    added FlashText component 11

--- components/Features/variants/Features.shuffle.01.jsx
+++ components/Features/variants/Features.shuffle.01.jsx
@@ -4,7 +4,7 @@ import {
 import Link from "next/link"
 import { useInView } from "react-intersection-observer"
 import Feature from "../FeatureCard_01"
-
+import FlashText from "../../Options/FlashText"
 const container = {
    hidden: { opacity: 0 },
    visible: {
