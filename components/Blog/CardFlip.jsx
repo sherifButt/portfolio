ce9c83f6c0998c9b5c67ef835b54cd911220fcc7
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-03-01 16:02:17 +0000

    fixed flipped card safari glitching issue

--- components/Blog/CardFlip.jsx
+++ components/Blog/CardFlip.jsx
@@ -35,22 +35,23 @@ const Card = ({
       if (isOff) setIsFlipped(!isFlipped);
    };
    const handleMouseLeave = () => {
-     
-      if ( isOff ) setIsFlipped( !isFlipped );
-      
+      if (isOff) setIsFlipped(!isFlipped);
    };
    const handleCrossClick = () => {
-      
-      setIsFlipped( !isFlipped );
-      setIsOff(!isOff)
-   }
+      setIsFlipped(!isFlipped);
+      setIsOff(!isOff);
+   };
    return (
       <ReactCardFlip
          isFlipped={isFlipped}
          flipDirection="horizontal">
          {/* Flip front side */}
          <div className=" flex flex-col rounded-lg shadow-lg overflow-hidden  dark:bg-gray-800 bg-white bg-clip-padding backdrop-filter backdrop-blur-xl dark:bg-opacity-30 bg-opacity-40">
-            <img
+            {isFlipped&&<p className="invisible">
+               .............................................................................................................................
+               .....
+            </p>}
+           {!isFlipped&& <img
                onMouseEnter={handleMouseEnter}
                onClick={handleMouseEnter}
                className="h-96 -mb-2 w-full object-cover "
@@ -60,37 +61,41 @@ const Card = ({
                      : imageUrl
                }
                alt=""
-            />
-            <p className=" text-sm p-1 mt-1 font-medium dark:text-indigo-400  text-indigo-600 hidden md:inline-block ">
-               {Array.isArray(category) &&
-                  category.filter(Boolean).map(cat => (
+            />}
+            { !isFlipped && <p className=" text-sm p-1 mt-1 font-medium dark:text-indigo-400  text-indigo-600 hidden md:inline-block ">
+               { Array.isArray( category ) &&
+                  category.filter( Boolean ).map( cat => (
                      <a
-                        key={cat.title}
-                        href={cat.href}
-                        alt={cat.alt}
-                        className={classNames(
+                        key={ cat.title }
+                        href={ cat.href }
+                        alt={ cat.alt }
+                        className={ classNames(
                            cat.color,
                            "hover:underline bg-transparent  mt-2 inline-flex items-center  px-3 py-0.5 rounded-full text-sm font-medium "
-                        )}>
+                        ) }>
                         <svg
                            xmlns="http://www.w3.org/2000/svg"
                            width="24"
                            height="24"
                            viewBox="0 0 24 24"
                            className="fill-indigo-00 dark:fill-indigo-100 mr-1 drop-shadow-sm ">
-                           <path d={cat.icon} />
+                           <path d={ cat.icon } />
                         </svg>
                         &nbsp;
                      </a>
-                  ))}
-            </p>
+                  ) ) }
+            </p> }
          </div>
          {/* flip back side */}
          <div
             key={posts ? posts[displayedPost].title : title}
-            className="flex h-auto flex-col rounded-lg shadow-lg overflow-hidden  dark:bg-gray-800 bg-white bg-clip-padding backdrop-filter backdrop-blur-xl dark:bg-opacity-30 bg-opacity-40 border dark:border-0  border-gray-100 dark:border-gray-800"
-            onMouseLeave={handleMouseLeave}>
-            <div className="flex-shrink-0">
+            className="react-card-back flex h-auto flex-col rounded-lg shadow-lg overflow-hidden  dark:bg-gray-800 bg-white bg-clip-padding backdrop-filter backdrop-blur-xl dark:bg-opacity-30 bg-opacity-40 border dark:border-0  border-gray-100 dark:border-gray-800"
+            onMouseLeave={ handleMouseLeave }>
+            {!isFlipped&&<p className="invisible">
+               .............................................................................................................................
+               .....
+            </p>}
+            {isFlipped&&<div className="flex-shrink-0">
                <div className="ml-auto p-3 absolute right-0">
                   <div className="-mx-1.5 -my-1.5">
                      <button
@@ -114,8 +119,8 @@ const Card = ({
                   }
                   alt=""
                />} */}
-            </div>
-            <div className="flex-1  p-6 flex flex-col justify-between ">
+            </div>}
+            {isFlipped&&<div className="flex-1  p-6 flex flex-col justify-between ">
                <div className="flex-1">
                   <p className="text-sm font-medium dark:text-indigo-400  text-indigo-600 hidden md:inline-block ">
                      {Array.isArray(category) &&
@@ -195,7 +200,7 @@ const Card = ({
                           </Link>
                        ))}
                </span>
-            </div>
+            </div>}
          </div>
       </ReactCardFlip>
    );
